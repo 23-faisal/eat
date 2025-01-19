7fa11e376666fafa9e45d05576157a2344fd98d7
23-faisal <a.faisal23r@gmail.com>
2025-01-19 15:45:47 +0600

    create frontend boilerplate and installed shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>Click the button</Button>
+    </>
+  );
+}
+
+export default App;
