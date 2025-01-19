179b881d1f0c8cbaca2888dcaf096260a207d606
23-faisal <a.faisal23r@gmail.com>
2025-01-19 17:26:17 +0600

    connected to the mognodb database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT;
+const port = parseInt(process.env.PORT as string, 10);
+const mongoUri = process.env.MONGODB_CONNECTION_STRING as string;
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log(`MongoDB is connected!`);
+  })
+  .catch((err) => {
+    process.exit(1);
+    console.error("MongoDB connection error:", err);
+  });
 
 app.get("/test", (req: Request, res: Response) => {
   res.status(200).json({
